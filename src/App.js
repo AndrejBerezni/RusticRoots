0d7ac512af3442d81681e9ca2b481c05b94286e4
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-06-20 16:20:22 +0100

    fix navbar styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from './Components/Navbar/Navbar';
-import Home from './Components/Home/Home';
-import Shop from './Components/Shop/Shop';
-import Account from './Components/Account/Account';
-import {Routes, Route} from "react-router-dom"
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+import Shop from "./Components/Shop/Shop";
+import Account from "./Components/Account/Account";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Routes>
-        <Route path='/' element = {<Home />} />
-        <Route path='/shop' element = {<Shop />} />
-        <Route path='/account' element = {<Account />} />
-        <Route path='*' element = {<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </div>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import './Navbar.css';
-import { Nav, Navbar, Container} from "react-bootstrap";
-import { NavLink }from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from "react-router-dom";
 
 export default function Navigation() {
     return (
-<Navbar>
-    <Container  className="navigation justify-content-space-between">
-        <Navbar.Brand href='/'>Rustic Roots Honey & Cider</Navbar.Brand>
-        <Nav className='gap-5'>
-            <Nav.Link to={'/'} as={NavLink} className='nav-link'>Home</Nav.Link>
-            <Nav.Link to={'/shop'} as={NavLink} className='nav-link'>Shop</Nav.Link>
-            <Nav.Link to={'/account'} as={NavLink} className='nav-link'>Account</Nav.Link>
-        </Nav>
-    </Container>
-</Navbar>
+        <Navbar expand='md' sticky="top">
+            <Container className="navigation justify-content-between">
+                <Navbar.Brand href='/' className="nav-brand">Rustic Roots Honey & Cider</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" id='navbar-toggle'/>
+                <Navbar.Collapse id='basic-navbar-nab'>
+                    <Nav className='gap-5 ms-auto'>
+                        <Nav.Link to={'/'} as={NavLink} className='nav-link'>Home</Nav.Link>
+                        <Nav.Link to={'/shop'} as={NavLink} className='nav-link'>Shop</Nav.Link>
+                        <Nav.Link to={'/account'} as={NavLink} className='nav-link'>Account</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
