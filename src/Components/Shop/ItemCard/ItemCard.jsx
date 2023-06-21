eec7b768d51d9a5f2c4e7faef5f13167f68416ab
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-06-21 11:11:24 +0100

    create and style product card component

---
+++ src/Components/Shop/ItemCard/ItemCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './ItemCard.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import cart from '../../../Assets/shopping-cart.png'
+
+export default function ItemCard({ img, title, size, description }) {
+    return (
+        <Card className='product-card' style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={img} className='card-img'/>
+            <Card.Body>
+                <Card.Title className='product-card-title'>{title}</Card.Title>
+                <Card.Text className='product-card-description'>{description}</Card.Text>
+                <Card.Text className='product-card-size'>{size}</Card.Text>
+                <div className="card-btn-div d-flex justify-content-center">
+                    <Button className='card-btn-decrement' >-</Button>
+                    <div className="item-number">5</div>
+                    <Button className='card-btn-increment'>+</Button>
+                </div>
+                <Button className="add-btn"><img alt='cart' className='btn-img' src={cart}/>Add to cart</Button>
+            </Card.Body>
+        </Card>
+    )
+}
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-import './Shop.jsx';
+import './Shop.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import ItemCard from "./ItemCard/ItemCard";
+import products from "../../Data/Products";
 
 export default function Shop() {
+    const honey = products.filter(product => product.type === 'honey');
+    const cider = products.filter(product => product.type === 'cider')
     return (
-        <div>
-            <h1>Shop</h1>
-        </div>
+            <Container className='shop py-5'>
+                <h1>Honey</h1>
+                <Row className="gap-3 mb-5">
+                {honey.map(product => (
+                    <ItemCard img={product.src} title={product.name} size={product.size} description={product.description} />
+                ))}
+                </Row>
+                <h1>Apple Cider</h1>
+                <Row className="gap-3 mb-5">
+                {cider.map(product => (
+                    <ItemCard img={product.src} title={product.name} size={product.size} description={product.description} />
+                ))}
+                </Row>
+            </Container>
     )
 }
