39642a10373dd552c99f053cf4aff1c60deeff57
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-06-22 15:19:49 +0100

    Handle adding same item again to the cart - increase count instead of adding new object

--- src/ReduxReducers/cartReducer.js
+++ src/ReduxReducers/cartReducer.js
@@ -1,12 +1,23 @@
-const initialState = []
+const initialState = [];
 
 const cartReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "ADD ITEM":
-            return [...state, action.payload];
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case "ADD ITEM":
+      // Check if item is already added
+      const index = state.findIndex(
+        (object) => object.name === action.payload.name
+      );
+      //   If it is, just increase the count
+      if (index > -1) {
+        const updatedState = [...state];
+        updatedState[index].count += action.payload.count;
+        return updatedState;
+      } else {
+        return [...state, action.payload];
+      }
+    default:
+      return state;
+  }
+};
 
-export default cartReducer
+export default cartReducer;
